558e743473326e208cc7cd86a1d1b12e9caa56c4
khafidhkayyis <khafidhkayyis@gmail.com>
2023-12-11 12:49:35 +0700

    dynamic routing

---
+++ my-app/src/pages/product/[id].tsx
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+const DetailProductPage = () => {
+    const { query } = useRouter();  // Query Adalah parameter dari userouter 
+
+    return (
+        <div>
+            <h1>Detail Product</h1>
+            <p>Product : {query.id} </p> {/* text id mengikuti nama file yang didalam kurung [] */}
+        </div>
+    )
+}
+
+export default DetailProductPage;
