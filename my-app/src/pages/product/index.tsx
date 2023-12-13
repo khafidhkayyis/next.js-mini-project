5740d1dbbe51e8ea0a578b0660a52f1f4a2601e7
khafidhkayyis <khafidhkayyis@gmail.com>
2023-12-13 14:54:12 +0700

    validation login at product

--- my-app/src/pages/product/index.tsx
+++ my-app/src/pages/product/index.tsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
 const ProductPage = () => {
+    const [IsLogin, setIsLogin] = useState(false);
+    const { push } = useRouter();
+
+    useEffect(() => {
+        if (!IsLogin) {
+            push("/auth/login");
+        }
+    }, []);
     return (
         <div>
             <h1>Product Page</h1>
